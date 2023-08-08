2f57737d67e26cc524e583f564b9ea3446c4163f
Nathan Chevalier <cerberusgfx@gmail.com>
2023-08-08 09:35:31 -0500

    Orders with prices displaying

--- scripts/orders.js
+++ scripts/orders.js
@@ -1,10 +1,13 @@
 export const getOrders = async () => {
-  const response = await fetch("http://localhost:8088/orders");
+  const response = await fetch(
+    "http://localhost:8088/orders?_expand=metal&_expand=style&_expand=size"
+  );
   const orders = await response.json();
 
   let html = `<h2> Current Orders</h2>`;
   const ordersArray = orders.map((order) => {
-    return `<div><section class="order"> Order #${order.id} </section></div>`;
+    const orderPrice = order.metal.price + order.style.price + order.size.price;
+    return `<div><section class="order"> Order #${order.id} costs $${orderPrice}</section></div>`;
   });
   html += ordersArray.join("");
 
